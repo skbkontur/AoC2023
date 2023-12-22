0703085caacb85494f19171e280b89f9d1fe0d95
spaceorc <spaceorc@yandex.ru>
2023-12-22 12:51:52 +0000

    Day 14 & 19

---
+++ day-19/PartChecker_ILGenerated.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Emit;
+
+namespace aoc;
+
+public class PartChecker_ILGenerated : IPartChecker
+{
+    private readonly Func<long[], bool> isAccepted;
+    
+    public PartChecker_ILGenerated(Rule[] rules)
+    {
+        var method = new DynamicMethod(
+            Guid.NewGuid().ToString(),
+            typeof(bool),
+            [typeof(long[])],
+            typeof(PartChecker_ILGenerated).Module,
+            true);
+
+        var il = method.GetILGenerator();
+        var labels = rules.ToDictionary(x => x.Name, _ => il.DefineLabel());
+        labels.Add("A", il.DefineLabel());
+        labels.Add("R", il.DefineLabel());
+        
+        il.Emit(OpCodes.Br, labels["in"]);
+
+        foreach (var (name, branches, otherwise) in rules)
+        {
+            il.MarkLabel(labels[name]);
+            foreach (var (field, op, value, next) in branches)
+            {
+                // stack: []
+                il.Emit(OpCodes.Ldarg_0); // [part]
+                il.Emit(OpCodes.Ldc_I4, field); // [part, field]
+                il.Emit(OpCodes.Ldelem_I8); // [part[field]]
+                il.Emit(OpCodes.Ldc_I8, value); // [part[field], value]
+                if (op == '<')
+                    il.Emit(OpCodes.Blt, labels[next]); // if (part[field] < value) goto next; []
+                else // if (op == '>')
+                    il.Emit(OpCodes.Bgt, labels[next]); // if (part[field] > value) goto next; []
+            }
+            il.Emit(OpCodes.Br, labels[otherwise]);
+        }
+        
+        // stack: []
+        il.MarkLabel(labels["A"]);
+        il.Emit(OpCodes.Ldc_I4_1); // [1 === true]
+        il.Emit(OpCodes.Ret);
+        
+        // stack: []
+        il.MarkLabel(labels["R"]);
+        il.Emit(OpCodes.Ldc_I4_0); // [0 === false]
+        il.Emit(OpCodes.Ret);
+
+        isAccepted = method.CreateDelegate<Func<long[], bool>>();
+    }
+
+    public bool IsAccepted(long[] part) => isAccepted(part);
+}
---
+++ day-19/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace aoc;
+
+public static class Program
+{
+    public static long SolvePart1(IPartChecker partChecker, long[][] parts)
+    {
+        return parts
+            .Where(partChecker.IsAccepted)
+            .Sum(part => part.Sum());
+    }
+
+    public static void Main()
+    {
+        var (rules, parts) = LoadFromFile("day19.txt");
+
+        var partChecker = new PartChecker(rules);
+        var partChecker_ILGenerated = new PartChecker_ILGenerated(rules);
+        
+        if (SolvePart1(partChecker, parts) != 575412)
+             throw new Exception("Bad code");
+        
+        if (SolvePart1(partChecker_ILGenerated, parts) != 575412)
+             throw new Exception("Bad code");
+        
+        var t = Stopwatch.GetTimestamp();
+        SolvePart1(partChecker, parts);
+        Console.WriteLine(Stopwatch.GetElapsedTime(t));
+        
+        var t2 = Stopwatch.GetTimestamp();
+        SolvePart1(partChecker_ILGenerated, parts);
+        Console.WriteLine(Stopwatch.GetElapsedTime(t2));
+    }
+
+    # region Parsing
+    
+    private static (Rule[] rules, long[][] parts) LoadFromFile(string fileName)
+    {
+        var input = File.ReadAllText(fileName).Split("\n\n");
+        var rulesInput = input[0].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+        var partsInput = input[1].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var rules = rulesInput
+            .Select(r => r.Split("{},".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+            .Select(r => (name: r[0], branches: r.Skip(1).SkipLast(1).Select(b => b.Split(':')).ToArray(), otherwise: r[^1]))
+            .Select(r => new Rule(
+                r.name,
+                r.branches.Select(b => new Branch("xmas".IndexOf(b[0][0]), b[0][1], long.Parse(b[0][2..]), b[1])).ToArray(), r.otherwise))
+            .ToArray();
+
+        var parts = partsInput
+            .Select(p => p
+                .Split("{}=xmas,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray()
+            )
+            .ToArray();
+
+        return (rules, parts);
+    }
+    
+    # endregion
+}
